ba9d92503e93b38e9034b7830126def13a487648
Akindolie Oluwatayo <akindolietayo@gmail.com>
2020-02-21 18:47:51 +0100

    add react component snippet

---
+++ .jsx
@@ -0,0 +1,15 @@
+{
+	"React Component": {
+		"prefix": "react-component",
+		"body": [
+			"import React from 'react';",
+			"",
+			"const ${TM_DIRECTORY/^.+\\/(.*)$/$1/} = () => {",
+			"\t$2 return <div>content</div>;",
+			"}",
+			"",
+			"export default  ${TM_DIRECTORY/^.+\\/(.*)$/$1/};"
+		],
+		"description": "scaffold react component"
+	}
+}
